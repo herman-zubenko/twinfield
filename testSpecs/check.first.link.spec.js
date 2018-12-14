c9f695fc427b0e412bd171569439b8bfcf382295
herman.zibenko <herman.zubenko@softheme.com>
2018-12-14 22:45:03 +0200

    specs comments

--- testSpecs/check.first.link.spec.js
+++ testSpecs/check.first.link.spec.js
@@ -3,7 +3,7 @@ var SearchPage = require('../pageObjects/google.search.page');
 describe('First result link', function() {
     var question = new SearchPage();
 
-    it('should point to twinfield.co.uk', async function() { //this scenario is failed constantly because the first link is a link to jobs.dou.ua
+    it('should point to twinfield.co.uk', async function() { //this test case is failed constantly because the first link is a link to jobs.dou.ua
 
         await question.openPage('https://www.google.com/');
         await question.askQuestion('Twinfield');
--- testSpecs/follow.links.spec.js
+++ testSpecs/follow.links.spec.js
@@ -1,7 +1,7 @@
 var SearchPage = require('../pageObjects/google.search.page');
 var ResultsPage = require('../pageObjects/google.results.page');
 
-describe('All links which contains Twinfield ', function() {
+describe('All links which contains Twinfield ', function() { //this test case opens new tabs with links which contains "Twinfield"
     var question = new SearchPage();
     var results = new ResultsPage();
 
