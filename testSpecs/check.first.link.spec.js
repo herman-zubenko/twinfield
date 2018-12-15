2addc316662ecd150076ebac7dcce64da48167c7
herman.zibenko <herman.zubenko@softheme.com>
2018-12-15 12:02:48 +0200

    const

--- testSpecs/check.first.link.spec.js
+++ testSpecs/check.first.link.spec.js
@@ -1,14 +1,14 @@
-var SearchPage = require('../pageObjects/google.search.page');
+const SearchPage = require('../pageObjects/google.search.page');
 
 describe('First result link', function() {
-    var question = new SearchPage();
+    const question = new SearchPage();
 
     it('should point to twinfield.co.uk', async function() { //this test case is failed constantly because the first link is a link to jobs.dou.ua
 
         await question.openPage('https://www.google.com/');
         await question.askQuestion('Twinfield');
         await question.pressEnterButton();
-        let link = await question.getFirstLink();
+        const link = await question.getFirstLink();
         await question.assertLink(link);
 
     });
--- testSpecs/follow.links.spec.js
+++ testSpecs/follow.links.spec.js
@@ -1,16 +1,16 @@
-var SearchPage = require('../pageObjects/google.search.page');
-var ResultsPage = require('../pageObjects/google.results.page');
+const SearchPage = require('../pageObjects/google.search.page');
+const ResultsPage = require('../pageObjects/google.results.page');
 
 describe('All links which contains Twinfield ', function() { //this test case opens new tabs with links which contains "Twinfield"
-    var question = new SearchPage();
-    var results = new ResultsPage();
+    const question = new SearchPage();
+    const results = new ResultsPage();
 
     it('should be opened', async function() {
         await question.openPage('https://www.google.com/');
         await question.askQuestion('Twinfield');
         await question.pressEnterButton();
-        let array = await results.getAllLinks();
-        let sortedArray = await results.sortLinksArray(array, 'twinfield');
+        const array = await results.getAllLinks();
+        const sortedArray = await results.sortLinksArray(array, 'twinfield');
         await results.followSortedLinks(sortedArray);
     });
 });
